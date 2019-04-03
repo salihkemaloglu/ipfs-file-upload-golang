2f3252268fb5ecb9a03c4c5fd7ed016543bf93d9
salih kemaloğlu <salihkem@outlook.com>
2019-04-03 23:41:16 +0300

    setup ssl to server

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"github.com/spf13/pflag"
+	"google.golang.org/grpc/metadata"
 
 	"io"
 	"os"
@@ -10,18 +12,12 @@ import (
 	"context"
 	"net/http"
 	"path/filepath"
-	"github.com/rs/cors"
-	"github.com/go-chi/chi"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/grpc/grpclog"
-	"google.golang.org/grpc/metadata"
-	chiMiddleware "github.com/go-chi/chi/middleware"
 	"github.com/improbable-eng/grpc-web/go/grpcweb"
 	"github.com/salihkemaloglu/gignox-main-beta-001/proto"
-	"github.com/salihkemaloglu/gignox-main-beta-001/middleware"
-	"github.com/salihkemaloglu/gignox-main-beta-001/proxy"
 	// "github.com/salihkemaloglu/DemMain-beta-001/ipfs"
 	//"github.com/salihkemaloglu/DemMain-beta-001/rabbitmq"
 )
@@ -123,32 +119,86 @@ func (*server) UploadFile(stream gigxMN.GigxMNService_UploadFileServer) error {
 func (s *server) InsertFile(ctx context.Context, req *gigxMN.InsertFileRequest) (*gigxMN.InsertFileResponse, error) {
 	return nil,nil
 }
+var (
+	
+	flagAllowAllOrigins = pflag.Bool("allow_all_origins", true, "allow requests from any origin.")
+	flagAllowedOrigins  = pflag.StringSlice("allowed_origins", nil, "comma-separated list of origin URLs which are allowed to make cross-origin requests.")
+
+	// useWebsockets = pflag.Bool("use_websockets", false, "whether to use beta websocket transport layer")
+	enableTls       = pflag.Bool("enable_tls", true, "Use TLS - required for HTTP2.")
+	tlsCertFilePath = pflag.String("tls_cert_file", "ssl/fullchain.pem", "Path to the CRT/PEM file.")
+	tlsKeyFilePath  = pflag.String("tls_key_file", "ssl/privkey.pem", "Path to the private key file.")
+	// flagHttpMaxWriteTimeout = pflag.Duration("server_http_max_write_timeout", 10*time.Second, "HTTP server config, max write duration.")
+	// flagHttpMaxReadTimeout  = pflag.Duration("server_http_max_read_timeout", 10*time.Second, "HTTP server config, max read duration.")
+)
 func main(){
+
+	pflag.Parse()
+
+	port :=8902
+	if *enableTls {
+		port = 8903
+	}
+
 	fmt.Println("Main Service Started")
+
 	opts := []grpc.ServerOption{}
 	grpcServer := grpc.NewServer(opts...)
 	gigxMN.RegisterGigxMNServiceServer(grpcServer, &server{})
 
-	wrappedGrpc := grpcweb.WrapServer(grpcServer)
+	allowedOrigins := makeAllowedOrigins(*flagAllowedOrigins)
 
-	router := chi.NewRouter()
-	router.Use(
-		chiMiddleware.Logger,
-		chiMiddleware.Recoverer,
-		middleware. NewGrpcWebMiddleware(wrappedGrpc).Handler,// Must come before general CORS handling
-		cors.New(cors.Options{
-			AllowedOrigins:   []string{"*"},
-			AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-			AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-			ExposedHeaders:   []string{"Link"},
-			AllowCredentials: true,
-			MaxAge:           300, // Maximum value not ignored by any of major browsers
-		}).Handler,
-	)
-
-	router.Get("/article-proxy", proxy.Article)
-
-	if err := http.ListenAndServe(":8900", router); err != nil {
-		grpclog.Fatalf("failed starting http2 server: %v", err)
+	options := []grpcweb.Option{
+		grpcweb.WithCorsForRegisteredEndpointsOnly(false),
+		grpcweb.WithOriginFunc(makeHttpOriginFunc(allowedOrigins)),
 	}
+
+	wrappedGrpc := grpcweb.WrapServer(grpcServer, options...)
+	handler := func(resp http.ResponseWriter, req *http.Request) {
+		wrappedGrpc.ServeHTTP(resp, req)
+	}
+
+	httpServer := http.Server{
+		Addr:    fmt.Sprintf(":%v", port),
+		Handler: http.HandlerFunc(handler),
+	}
+
+	grpclog.Printf("Starting server. http port: %d, with TLS: %v", port, *enableTls)
+
+	if *enableTls {
+		fmt.Printf("server started as  https and listen to port: %v \n",port)
+		if err := httpServer.ListenAndServeTLS(*tlsCertFilePath, *tlsKeyFilePath); err != nil {
+			grpclog.Fatalf("failed starting http2 server: %v", err)
+		}
+	} else {
+		fmt.Printf("server started as http and listen to port: %v \n",port)
+		if err := httpServer.ListenAndServe(); err != nil {
+			grpclog.Fatalf("failed starting http server: %v", err)
+		}
+	}
+}
+func makeHttpOriginFunc(allowedOrigins *allowedOrigins) func(origin string) bool {
+	if *flagAllowAllOrigins {
+		return func(origin string) bool {
+			return true
+		}
+	}
+	return allowedOrigins.IsAllowed
+}
+func makeAllowedOrigins(origins []string) *allowedOrigins {
+	o := map[string]struct{}{}
+	for _, allowedOrigin := range origins {
+		o[allowedOrigin] = struct{}{}
+	}
+	return &allowedOrigins{
+		origins: o,
+	}
+}
+
+type allowedOrigins struct {
+	origins map[string]struct{}
+}
+func (a *allowedOrigins) IsAllowed(origin string) bool {
+	_, ok := a.origins[origin]
+	return ok
 }
